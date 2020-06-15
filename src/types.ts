1fd5874163aff7f2cd44d694344498ace434f005
Hiroki Osame <hiroki.osame@gmail.com>
2020-06-15 02:45:29 -0400

    Moved to TS

---
+++ src/types.ts
@@ -0,0 +1,20 @@
+export interface Options {
+	verbose: boolean;
+	output: string;
+};
+
+export interface Dependencies {
+	[key: string]: string[]
+}
+
+export interface UnsedDependencies {
+	dependencies: string[];
+	devDependencies: string[];
+}
+
+export type UsedDependencies = Dependencies | string[];
+
+export interface Result {
+	usedDependencies: UsedDependencies;
+	unusedDependencies: UnsedDependencies;
+}
