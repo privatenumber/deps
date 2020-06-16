6a8a0ca418d4d77891e838f039df0087b86b0fdc
hiroki <hiroki.osame@gmail.com>
2020-06-16 02:43:58 -0400

    Revert "refactor: use cac over minimist"

--- src/bin.ts
+++ src/bin.ts
@@ -1,27 +1,28 @@
-import {cac} from 'cac';
+import minimist from 'minimist';
+import {showHelp} from './utils';
 import deps from '.';
 
 (async () => {
-	const cli = cac();
+	const {help, version, verbose, output, _} = minimist(process.argv.slice(2), {
+		boolean: [
+			'version',
+			'verbose',
+			'help',
+		],
+		alias: {
+			verbose: 'v',
+			output: 'o',
+			help: 'h',
+		},
+	});
 
-	cli.help();
+	if (help || version) {
+		return showHelp();
+	}
 
-	const {version} = require('../package'); // eslint-disable-line @typescript-eslint/no-var-requires
-	cli.version(version);
+	const cmd = _.join(' ');
 
-	cli
-		.command('<...command>', 'Records and analyzes the dependency usage of a given command')
-		.example('deps node file.js')
-		.option('-v, --verbose', 'verbose mode - Output as an object with specific files')
-		.option('--output <dest>', 'target destination for JSON')
-		.action((command, {verbose, output}) => {
-			void deps(command.join(' '), {verbose, output});
-		});
-
-	// Cli
-	// 	.command('analyze <JSON file>');
-
-	cli.parse();
+	return deps(cmd, {verbose, output});
 })().catch(error => {
 	console.log('Error:', error.message);
 });
